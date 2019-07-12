7be1ca3c501d2824835a0e2073f40ddb789fb884
maguichisss <maguichisss@gmail.com>
2019-07-12 13:18:40 -0500

    Inicio, variables, enteros y strings JS

---
+++ tutorial.js
@@ -0,0 +1,27 @@
+// VARIABLES
+// las variables en programacion son algo parecido al algebra, pero muuuucho mas sencillo
+// de lo que nos hicieron creer, ademas las variables solo pueden empezar con letras o 
+// guion bajo
+// Se puede inicializar una variable de la siguiente manera:
+
+var a = 5; // una 'sentencia' en javascript (JS) puede o no terminar con ';' es opcional
+var b
+b = 10
+x = a + b // esto como podras darte cuenta es una feeea ecuacion, pero es bastante simple
+_x = x + x
+
+// cual crees que sea el valor de 'x' ???
+console.log(x)
+console.log(_x)
+// si sumamos a+b el resultado es 15
+
+// STRINGS
+// los 'strings' (en ingles) o cadenas, es texto basicamente, y se pueden 'concatenar' (unir)
+// de la siguiente forma:
+sustantivo = "perro"
+verbo = "corriendo"
+frase = "El "+sustantivo+" esta "+verbo
+console.log(frase)
+// en JS se pueden concatenar numeros y strings de forma sencilla (en otros lenguajes se hace
+// de otra manera) de la siguiente forma:
+console.log(frase+" a "+x+" kilometros por hora") // utilizamos la 'x' del ejemplo de arriba
